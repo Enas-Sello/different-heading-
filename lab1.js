1cd1e8aaba5ca2b6fb4fa254f763f8444a70c35d
Enas Sello <94912513+Enas-Sello@users.noreply.github.com>
2022-05-27 23:47:55 +0200

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,12 @@
+var prompt = window.prompt("write some words " );
+for (var i = 1; i <=6 ; i++)
+document.write("<h"+ i +">" + prompt + "<h" + i +">");
+
+
+// var inputNumber = 0 ;
+// do{
+//     var numInput =parseInt( prompt("enter your  numbers"));
+//     inputNumber += numInput
+// }
+// while(numInput != 0 && inputNumber < 100 );
+// document.write("<h1> the sum of your input is : " + inputNumber  +"<h1>")
